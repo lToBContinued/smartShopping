24e10b8044f5213be70fb686e063feab5f5c27b2
lToBContinued <3212834434@qq.com>
2024-04-09 10:41:53 +0800

    4.完成了首页静态页面布局

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Switch, Rate, Tabbar, TabbarItem, NavBar, Toast } from 'vant'
+import { Button, Switch, Rate, Tabbar, TabbarItem, NavBar, Toast, Search, Swipe, SwipeItem, Grid, GridItem } from 'vant'
 // 按需导入
 Vue.use(Button)
 Vue.use(Switch)
@@ -8,3 +8,8 @@ Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(NavBar)
 Vue.use(Toast)
+Vue.use(Search)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Grid)
+Vue.use(GridItem)
--- src/views/layout/home.vue
+++ src/views/layout/home.vue
@@ -1,13 +1,106 @@
 <template>
-  <div>我是home</div>
+  <div class="home">
+    <!--顶部导航栏-->
+    <van-nav-bar title="智慧商城" fixed></van-nav-bar>
+
+    <!--搜索框-->
+    <van-search v-model="value" placeholder="请输入搜索关键词" shape="round"/>
+
+    <!--轮播图-->
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item><img src="@/assets/banner1.jpg" alt=""></van-swipe-item>
+      <van-swipe-item><img src="@/assets/banner2.jpg" alt=""></van-swipe-item>
+      <van-swipe-item><img src="@/assets/banner3.jpg" alt=""></van-swipe-item>
+    </van-swipe>
+
+    <!--商品分类-->
+    <van-grid :border="false" :column-num="5">
+      <van-grid-item
+        v-for="value in 10"
+        :key="value"
+        icon="http://cba.itlike.com/public/uploads/10001/20230320/58a7c1f62df4cb1eb47fe83ff0e566e6.png"
+        text="新品首发"
+        @click="$router.push('/category')"
+      />
+    </van-grid>
+
+    <!--主会场-->
+    <div class="main">
+      <img src="@/assets/main.png" alt="">
+    </div>
+    <!--猜你喜欢-->
+    <div class="guess">
+      <p class="guess-title">—— 猜你喜欢 ——</p>
+      <div class="goods-list">
+        <GoodsItem v-for="item in 10" :key="item"></GoodsItem>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import GoodsItem from '@/components/GoodsItem.vue'
+
 export default {
-  name: 'homeIndex'
+  name: 'homeIndex',
+  data () {
+    return {
+      value: ''
+    }
+  },
+  components: {
+    GoodsItem
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.home {
+  padding-top: 100px;
+  padding-bottom: 50px;
+}
 
+.van-nav-bar {
+  z-index: 999;
+  background-color: #c21401;
+
+  ::v-deep .van-nav-bar__title {
+    color: #fff;
+  }
+}
+
+.van-search {
+  position: fixed;
+  width: 100%;
+  top: 46px;
+  z-index: 999;
+}
+
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  text-align: center;
+
+  img {
+    width: 100%;
+  }
+}
+
+// 主会场
+.main img {
+  display: block;
+  width: 100%;
+}
+
+// 猜你喜欢
+.guess .guess-title {
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+}
+
+// 商品样式
+.goods-list {
+  background-color: #f6f6f6;
+}
 </style>
